4457a1c77547957b197584bcdab992f2bb491d64
Julio <julio.tamogami@gmail.com>
2024-12-10 10:53:20 -0300

    added first code to jwt token generator

--- FinanTrack.Auth/Program.cs
+++ FinanTrack.Auth/Program.cs
@@ -1,59 +1,30 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Abstractions;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.Resource;
+using FinanTrack.Auth.Helpers;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-builder.Services.AddAuthorization();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace FinanTrack.Auth
 {
-    app.MapOpenApi();
-}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+            // Add services to the container.
+           builder.Services.Configure<SecuritySettings>(builder.Configuration.GetSection("Security"));
 
-app.UseAuthentication();
-app.UseAuthorization();
+            builder.Services.AddControllers();
 
-var scopeRequiredByApi = app.Configuration["AzureAd:Scopes"] ?? "";
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+            var app = builder.Build();
 
-app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-{
-    //httpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+            // Configure the HTTP request pipeline.
 
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-//.WithOpenApi()
-//.RequireAuthorization();
+            app.UseHttpsRedirection();
 
-app.Run();
+            app.UseAuthorization();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
